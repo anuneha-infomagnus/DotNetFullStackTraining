51114b7ae2ec5581ada8b06f114db516219d6b44
Anuneha peteti <anuneha.peteti@infomanus.com>
2026-08-13 11:44:56 +0530

    Day 1 hands-on completed

---
+++ Day01/ClassLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary;
+
+public class Employee
+{
+    public void DisplayEmployee()
+    {
+        Console.WriteLine("Employee: Anuneha");
+    }
+}
---
+++ Day01/ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using ClassLibrary;
+
+Employee employee = new Employee();
+
+employee.DisplayEmployee();
